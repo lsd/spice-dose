a2fa12b555e838ad6cf55aa139158e1daa902a24
Isam <isam@imagineeasy.com>
2014-12-15 18:40:00 -0500

    Dont commit commented code. This is what git is for

--- server/startup/getQuotes.js
+++ server/startup/getQuotes.js
@@ -5,60 +5,7 @@ Meteor.startup(function () {
     getQuotes: function () {
       result = Meteor.http.get('http://en.wikiquote.org/wiki/Dune');
       $ = cheerio.load(result.content);
-
-      /*
-      var quotes = $('#mw-content-text ul li').text();
-      return quotes;
-      */
-
       var item = $('#mw-content-text ul li');
-
-      //  var test = $('#mw-content-text ul li').hasClass('toclevel-1');
-
-      /*
-      _.each(item, function(item) {
-        if (this.hasClass('toclevel-1')) {
-          this.remove();
-        } else {
-          var quoteText = this.text();
-          console.log(quoteText);
-          Quotes.insert({ quote: quoteText });
-        }
-      });
-      */
-      /*
-      if (Quotes.find().count() === 0) {
-        for (var i = 0; i < item.length; i++) {
-          if (item.hasClass('toclevel-1')) {
-            $(this).remove();
-          } else {
-            var quoteText = item[i].text();
-            console.log(quoteText);
-            Quotes.insert({ quote: quoteText });
-          }
-        }
-      }
-      */
-      /*
-      var items = $('#mw-content-text ul');
-      var item = $('#mw-content-text ul li');
-
-      _.each(items, function(item) {
-        if ($(item).hasClass('toclevel-1')) {
-          $(this).remove();
-        } else {
-          return $(item).text();
-        }
-      });
-
-      $(items).each(function() {
-        if ( $(this).hasClass('toclevel-1') ) {
-          $(this).remove();
-        } else {
-          return $(this).text();
-        }
-      });
-      */
     }
   });
 
